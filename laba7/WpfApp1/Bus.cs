93b12f7c171d1f8678318083e5f17ec58d400bd8
Evhenos <96354134+Evhenos@users.noreply.github.com>
2022-11-25 15:23:20 +0300

    Add files via upload

---
+++ laba7/WpfApp1/Bus.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Bus
+    {
+        public int Place { get; set; }
+        public int Price { get; set; }
+        public int PriceKof { get; set; }
+        public int FreePlase { get; set; }
+
+        List<string> Route = new List<string> { "Гомель", "Речица", "Светлогорск", "Жлобин", "Бобруйск" };
+        List<string> Station = new List<string>();
+
+
+        public Bus(int price, int plase)
+        {
+            Place = plase;
+            FreePlase = plase;
+            Price = price;
+        }
+
+        public bool StartTrip(string start, string end)
+        {
+            if (AddPssenger())
+            {
+                FreePlase--;
+                int Start = Route.IndexOf(start);
+                int End = Route.IndexOf(end);
+
+                Price = Price + End - Start;
+
+                Station.Add(end);
+
+                return true;
+            }
+
+            return false;
+        }
+        public bool AddPssenger()
+        {
+            if (FreePlase >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void NewStation(string station)
+        {
+            for(int i = 0; i < Station.Count; i++)
+            {
+                if(station == Station[i])
+                {
+                    Station.RemoveAt(i);
+                    DelPssenger();
+                    i--;
+                }
+            }
+        }
+
+        public void DelPssenger()
+        {
+            if (FreePlase < Place)
+            {
+                FreePlase++;
+            }
+        }
+
+        public string ShowInfo()
+        {
+            return $"Всего мест: {Place}\nСвободных мест: {FreePlase}\n{Station.Count}";
+        }
+    }
+}
---
+++ laba7/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Maz maz = new Maz(24, 15);
+        Man man = new Man(16, 10);
+        Mersedes mers = new Mersedes(20, 25);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            switch (bus_type.Text)
+            {
+                case "MAN":
+                    if (man.StartTrip((string)CityName.Content, endList.Text))
+                    {
+                        price.Content = $"Цена: {man.Price}";
+                    }
+                    else
+                    {
+                        price.Content = "Нет мест";
+                    }
+                    break;
+
+                case "MAZ":
+                    if (maz.StartTrip((string)CityName.Content, endList.Text))
+                    {
+                        price.Content = $"Цена: {man.Price}";
+                    }
+                    else
+                    {
+                        price.Content = "Нет мест";
+                    }
+                    break;
+
+                case "MERS":
+                    if (mers.StartTrip((string)CityName.Content, endList.Text))
+                    {
+                        price.Content = $"Цена: {man.Price}";
+                    }
+                    else
+                    {
+                        price.Content = "Нет мест";
+                    }
+                    break;
+
+            }
+
+            //Новая станция
+            switch (CityName.Content)
+            {
+                case "Гомель":
+                    CityName.Content = "Речица"; break;
+                case "Речица":
+                    CityName.Content = "Светлогорск"; break;
+                case "Светлогорск":
+                    CityName.Content = "Жлобин"; break;
+                case "Жлобин":
+                    CityName.Content = "Бобруйск"; break;
+                case "Бобруйск":
+                    CityName.Content = "Гомель"; break;
+            }
+
+            man.NewStation((string)CityName.Content);
+            maz.NewStation((string)CityName.Content);
+            mers.NewStation((string)CityName.Content);
+
+            man_info.Content = $"MAN\n{man.ShowInfo()}";
+            maz_info.Content = $"MAZ\n{maz.ShowInfo()}";
+            mers_info.Content = $"MERS\n{mers.ShowInfo()}";
+        }
+
+        private void bus_type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
